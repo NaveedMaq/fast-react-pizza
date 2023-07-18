a5bd90f17e355abce6ae6fba0f2970250331e88a
NaveedMaq <naveedmaqbool30@gmail.com>
2023-07-19 00:46:45 +0530

    feat:Style cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      ' inline-block  rounded-full border-2 border-stone-300 px-4 py-2.5 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800  focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-600 disabled:text-slate-300 md:px-6 md:py-3.5',
   };
 
   if (to)

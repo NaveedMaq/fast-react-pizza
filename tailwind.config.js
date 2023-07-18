a3fb27a582db9e78a9727f326a242209d2c58687
NaveedMaq <naveedmaqbool30@gmail.com>
2023-07-18 23:26:36 +0530

    feat: Add basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    extend: {
+      colors: {
+        pizza: '#123456',
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };

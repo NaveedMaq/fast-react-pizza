70f98d0d3ed9d1289cf9e9b002706e5bd58624f1
NaveedMaq <naveedmaqbool30@gmail.com>
2023-07-19 21:28:22 +0530

    feat: Make cart overview dynamic

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,60 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+  //   cart: [
+  //     {
+  //       pizzaId: 12,
+  //       name: 'Medditerannea',
+  //       quantity: 2,
+  //       unitPrice: 16,
+  //       totalPrice: 2 * 16,
+  //     },
+  //   ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      // payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    decreaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getTotalPizzaQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalPizzaPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);

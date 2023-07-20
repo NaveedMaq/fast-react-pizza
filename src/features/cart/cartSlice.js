9856487ea819387313fe4d9e23429ab854909642
NaveedMaq <naveedmaqbool30@gmail.com>
2023-07-20 09:25:33 +0530

    feat: Add delete to menu

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -60,3 +60,6 @@ export const getTotalPizzaQuantity = (state) =>
 
 export const getTotalPizzaPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
